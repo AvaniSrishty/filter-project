960db5e668002ad829f18da34405692764401e1f
AvaniSrishty <92068623+AvaniSrishty@users.noreply.github.com>
2022-07-30 17:34:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,8 +4,20 @@ function setup() {
     
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded)
+    poseNet.on('pose', gotposes);
+    }
+
+    function modelLoaded(){
+      console.log("model is ready");
     }
     
+   function gotposes(result){ 
+     if(result.length > 0){
+        console.log(result);
+     }
+    }
+
     function draw() {
     image(video, 0, 0, 350, 300);
     }
